0e32f6917180fdfd506749481f2faeb89e619458
Jonathan Duran <jonduran@outlook.com>
2020-05-17 23:25:39 -0400

    Updated weather codes

--- src/assets/common.js
+++ src/assets/common.js
@@ -30,86 +30,86 @@ export function weatherDecoder(id) {
 	switch(id) {
 		case (/2[0-9][0-9]/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'dark',
 				description: 'thunderstorm'
 			};
 			break;
 		case (/3[0-9][0-9]/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'medium',
 				description: 'drizzle'
 			};
 			break;
-			case (/50[2-4]/).test(id):
-				decoded = {
-					background: 'blue',
-					description: 'heavy rain'
-				};
-				break;
+		case (/50[2-4]/).test(id):
+			decoded = {
+				ambiance: 'dark',
+				description: 'heavy rain'
+			};
+			break;
 		case (/5[0-9][0-9]/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'medium',
 				description: 'rain'
 			};
 			break;
 		case (/6[0-9][0-9]/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'medium',
 				description: 'snow'
 			};
 			break;
 		case (/7[0,2,4]1/).test(id):
-				decoded = {
-					background: 'blue',
-					description: 'fog'
-				};
-				break;
+			decoded = {
+				ambiance: 'medium',
+				description: 'fog'
+			};
+			break;
 		case (/711/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'medium',
 				description: 'smoke'
 			};
 			break;
 		case (/7[3,5]1/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'medium',
 				description: 'sand'
 			};
 			break;
 		case (/76[1-2]/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'medium',
 				description: 'dust'
 			};
 			break;
 		case (/7[7-8]1/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'light',
 				description: 'strong winds'
 			};
 			break;
 		case (/800/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'light',
 				description: 'clear'
 			};
 			break;
 		case (/80[1-2]/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'light',
 				description: 'partly cloudy'
 			};
 			break;
 		case (/80[3-4]/).test(id):
 			decoded = {
-				background: 'blue',
+				ambiance: 'medium',
 				description: 'cloudy'
 			};
 			break;
 		default: 
 			decoded = {
-				background: 'darkblue',
-				description: 'error'
+				ambiance: 'light',
+				description: 'clear'
 			};
 			break;
 	}
