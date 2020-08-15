38a5193845ddb753b6096cf1152a2c9a461a332e
Jonathan Duran <jonduran@outlook.com>
2020-08-14 20:32:33 -0400

    Updates

---
+++ precache-manifest.1d194dbaa91c64fbc187a9683a8f020a.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719bfbcb5d0e34c16fd9ee7a4f64385e",
+    "url": "/WearWeather/index.html"
+  },
+  {
+    "revision": "f3e036e2b280246f997b",
+    "url": "/WearWeather/static/css/2.717d6787.chunk.css"
+  },
+  {
+    "revision": "2b30d85aa10a952c8b98",
+    "url": "/WearWeather/static/css/main.2c2dd66c.chunk.css"
+  },
+  {
+    "revision": "d19385993aa8cb6f295f",
+    "url": "/WearWeather/static/js/10.61830834.chunk.js"
+  },
+  {
+    "revision": "892415b4551e3ab6c3db",
+    "url": "/WearWeather/static/js/11.fdb6dd84.chunk.js"
+  },
+  {
+    "revision": "6d63d4a7dd9aa5973af4",
+    "url": "/WearWeather/static/js/12.0d1f868a.chunk.js"
+  },
+  {
+    "revision": "7d542ec0e840fe50a181",
+    "url": "/WearWeather/static/js/13.c22f8ba5.chunk.js"
+  },
+  {
+    "revision": "d3a412e2a96226a6c578",
+    "url": "/WearWeather/static/js/14.9ab8d83d.chunk.js"
+  },
+  {
+    "revision": "6f271f0e6cbca199d6f6",
+    "url": "/WearWeather/static/js/15.d2114664.chunk.js"
+  },
+  {
+    "revision": "95cc1859719cc430dc4c",
+    "url": "/WearWeather/static/js/16.b3ba6ef1.chunk.js"
+  },
+  {
+    "revision": "b9c2c67b74238f374436",
+    "url": "/WearWeather/static/js/17.2e769cb5.chunk.js"
+  },
+  {
+    "revision": "0fb4f6594669a04a7807",
+    "url": "/WearWeather/static/js/18.99c2d130.chunk.js"
+  },
+  {
+    "revision": "4a0d35574b2c95c4b7a5",
+    "url": "/WearWeather/static/js/19.eef9d09b.chunk.js"
+  },
+  {
+    "revision": "f3e036e2b280246f997b",
+    "url": "/WearWeather/static/js/2.db71300d.chunk.js"
+  },
+  {
+    "revision": "87762c6079e782ab8a5eaefd61461106",
+    "url": "/WearWeather/static/js/2.db71300d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67d910749ea1a58bfee",
+    "url": "/WearWeather/static/js/20.ceb2c211.chunk.js"
+  },
+  {
+    "revision": "037c2a006af7a8a267ed",
+    "url": "/WearWeather/static/js/21.994ce6e8.chunk.js"
+  },
+  {
+    "revision": "ccc2df31a83d5a930306",
+    "url": "/WearWeather/static/js/22.8c0c9dec.chunk.js"
+  },
+  {
+    "revision": "d8048b2f4668eefbbb32",
+    "url": "/WearWeather/static/js/23.0eb76900.chunk.js"
+  },
+  {
+    "revision": "7f4fd6e2365235b9dd30",
+    "url": "/WearWeather/static/js/24.58b91685.chunk.js"
+  },
+  {
+    "revision": "79b5267b4d6efa0368e2",
+    "url": "/WearWeather/static/js/25.513c1efa.chunk.js"
+  },
+  {
+    "revision": "074a2064ce3381248d08",
+    "url": "/WearWeather/static/js/3.94dd135c.chunk.js"
+  },
+  {
+    "revision": "7035da7f4ecdb556e412",
+    "url": "/WearWeather/static/js/4.b28a3a35.chunk.js"
+  },
+  {
+    "revision": "fa6a0ee49ea8903ad01a",
+    "url": "/WearWeather/static/js/5.a3e6c6a7.chunk.js"
+  },
+  {
+    "revision": "4b117567a6aed9458cb2",
+    "url": "/WearWeather/static/js/6.c93c5621.chunk.js"
+  },
+  {
+    "revision": "db3734402d3cd4ad2825",
+    "url": "/WearWeather/static/js/7.80cb7f38.chunk.js"
+  },
+  {
+    "revision": "3e535c242a08b258581e",
+    "url": "/WearWeather/static/js/8.e950d11a.chunk.js"
+  },
+  {
+    "revision": "ba209da3596ff09097bf",
+    "url": "/WearWeather/static/js/9.37d48374.chunk.js"
+  },
+  {
+    "revision": "2b30d85aa10a952c8b98",
+    "url": "/WearWeather/static/js/main.afee2c63.chunk.js"
+  },
+  {
+    "revision": "4b1d8fdf6404e6421093",
+    "url": "/WearWeather/static/js/runtime-main.c3eedfa5.js"
+  }
+]);
